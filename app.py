d81d94063ee17ec06605615385058806b267644e
Atyutskiy Maksim <macsim.1999@icloud.com>
2017-09-12 20:56:22 +0300

    minor changes

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ import vk
 from auth import VKAuth
 import config
 
+
 def getDialogSel(url):
     url_params = url.split('?')[1:][0].split('&')
     for item in url_params:
         if item.split('=')[0] == 'sel':
             return item.split('=')[1] if item.split('=')[1][0] != 'c' else item.split('=')[1][1:]
 
-Auth = VKAuth(['messages'], config.vk_app_id, '5.68')
+
+Auth = VKAuth(['messages, '], config.vk_app_id, '5.68')
 Auth.auth()
 
 access_token = Auth.get_token()
